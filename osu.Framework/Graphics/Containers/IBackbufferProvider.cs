fb60e9433cb75beb4794af157219b16c86f043dc
BoomboxRapsody <seonghyeon802@gmail.com>
2026-02-12 13:27:24 +0900

    Update framework for use `BackdropBlurContainer`

---
+++ osu.Framework/Graphics/Containers/IBackbufferProvider.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2026 BoomboxRapsody <boomboxrapsody@gmail.com>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+
+namespace osu.Framework.Graphics.Containers
+{
+    /// <summary>
+    /// A container which ensures that its children are drawn to a framebuffer.
+    /// </summary>
+    [Cached]
+    public interface IBackbufferProvider : IContainer
+    {
+    }
+}
